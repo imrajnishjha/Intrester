32e310b130ece487ee2e5592e0a0ea1c83c89535
Administrator <rajnishjha7890@gmail.com>
2023-02-22 08:33:35 +0000

    Entry Log

--- app/src/main/java/com/wormos/intrester/CreatePlace.java
+++ app/src/main/java/com/wormos/intrester/CreatePlace.java
@@ -6,6 +6,8 @@ import androidx.appcompat.widget.AppCompatButton;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
 public class CreatePlace extends AppCompatActivity {
 
     AppCompatButton createPlaceBtn;
@@ -16,6 +18,10 @@ public class CreatePlace extends AppCompatActivity {
         setContentView(R.layout.activity_create_place);
 
         createPlaceBtn = findViewById(R.id.create_place_btn);
-        createPlaceBtn.setOnClickListener(view -> startActivity(new Intent(this,addMember.class)));
+        createPlaceBtn.setOnClickListener(view->{
+            BottomSheetDialog bottomSheet = new BottomSheetDialog(CreatePlace.this);
+            bottomSheet.setContentView(R.layout.activity_add_new_place);
+            bottomSheet.show();
+        });
     }
 }
---
+++ app/src/main/java/com/wormos/intrester/addNewPlace.java
@@ -0,0 +1,18 @@
+package com.wormos.intrester;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class addNewPlace extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_place);
+    }
+}
